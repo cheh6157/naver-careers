dd99f8c7c98c76ea724ec56eb5fc23c5fe3ee790
choi eun ho <cheh6157@naver.com>
2022-07-11 01:06:58 +0900

    사이드메뉴 완성하기

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,10 +3,28 @@ $(function(){
     /*
      * 접근성 & UX 개선 (키보드 사용할때만 포커스 나오게)
      *
+     *
      * 
      /*
+     * gnb 호버 영역
+     * @version 1.0.0 |||| @since 2022-01-16 |||| @author 본인이름 (은호)
+     */
+     $('.header .gnb-item').hover(function(){ //헤더의 지엔비아이템을 호버했을시
+      if($(this).find('.sub-menu').length){ //만약 이것(지엔비아이템)에서 찾아라 서브메뉴에 길이가 있다면
+        $('.header').addClass('hover'); //헤더에 호버라는 클래스를 더해라
+        $(this).find('.sub-menu').addClass('active') //지엔비아이템에서 찾아라 서브메뉴를 거기에 액티브라는 클래스를 더해라
+      }
+    },function(){ //호버를 뗄경우
+      $('.header').removeClass('hover'); //헤더에 호버클래스를 제거해라
+      $('.sub-menu').removeClass('active') //서브메뉴에 액티브클래스를 제거해라
+    });
+
+
+
+
+     /*
      * 메인 슬라이드 영역
-     * @version 1.0.0 |||| @since 2022-01-16 |||| @author 본인이름 (Nico)
+     * @version 1.0.0 |||| @since 2022-01-16 |||| @author 본인이름 (은호)
      */
         var mainSlide = new Swiper(".video", {
        
@@ -22,7 +40,7 @@ $(function(){
     
      * 
      * sc-people 슬라이드 구역
-     * @version 1.0.0 |||| @since 2022-01-16 |||| @author 본인이름 (Nico)
+     * @version 1.0.0 |||| @since 2022-01-16 |||| @author 본인이름 (은호)
      *
      *
      */
